1fb6e6f57872e6a2bccd295de5c965acdb184971
jean-jacques Johnson <jeanjacques.johnson@gmail.com>
2019-10-11 16:06:57 -0700

    LayOut 1rst Commit
    Please review layout

---
+++ styles/headerStyle.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+var AppHeaderStyle = StyleSheet.create({
+    comp:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    TextStyle:{
+        color: 'white',
+        bottom: '20%'
+
+    },
+    background:{
+        width:'100%',
+        height:'100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+   
+ })
+
+export default AppHeaderStyle
